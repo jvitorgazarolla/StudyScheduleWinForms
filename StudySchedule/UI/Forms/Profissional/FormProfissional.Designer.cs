b99728d84563508e839ad96bd0652721c5633c1d
jvitorgazarolla <jgazarolla@gmail.com>
2026-02-18 22:22:00 -0300

    Apagando pastas deletadas porém que apontava no projeto, corrigindo o nome da pasta profissional

--- StudySchedule/UI/Forms/Profissional/FormProfissional.Designer.cs
+++ StudySchedule/UI/Forms/Profissional/FormProfissional.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace StudySchedule.UI.Forms.Cliente
+﻿namespace StudySchedule.UI.Forms.Profissiional
 {
-    partial class FormCliente
+    partial class FormProfissional
     {
         /// <summary>
         /// Required designer variable.
@@ -28,237 +28,268 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dgv_clientes = new DataGridView();
-            gpb_lista = new GroupBox();
-            pnl_edicao = new Panel();
-            comboBox1 = new ComboBox();
-            cb_genero = new ComboBox();
-            lbl_sexo = new Label();
-            textBox2 = new TextBox();
+            btn_buscar = new Button();
+            btn_novo = new Button();
+            txt_busca_profissional = new TextBox();
+            btn_limpar = new Button();
+            btn_cancelar = new Button();
+            btn_salvar = new Button();
+            lbl_titulo = new Label();
+            txt_telefone = new TextBox();
             lbl_telefone = new Label();
             lbl_nome = new Label();
             txt_nome = new TextBox();
-            button1 = new Button();
-            textBox1 = new TextBox();
-            lbl_titulo = new Label();
-            lbl_profissional = new Label();
-            cb_alergico = new CheckBox();
-            txt_produto_alergico = new TextBox();
-            gp_observacao = new GroupBox();
-            ((System.ComponentModel.ISupportInitialize)dgv_clientes).BeginInit();
-            gpb_lista.SuspendLayout();
+            pnl_edicao = new Panel();
+            check_status = new CheckBox();
+            cb_especialidade = new ComboBox();
+            lbl_especialidade = new Label();
+            gpb_lista = new GroupBox();
+            dgv_profissional = new DataGridView();
             pnl_edicao.SuspendLayout();
-            gp_observacao.SuspendLayout();
+            gpb_lista.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgv_profissional).BeginInit();
             SuspendLayout();
             // 
-            // dgv_clientes
+            // btn_buscar
             // 
-            dgv_clientes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgv_clientes.BackgroundColor = Color.White;
-            dgv_clientes.BorderStyle = BorderStyle.None;
-            dgv_clientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgv_clientes.Location = new Point(6, 22);
-            dgv_clientes.Name = "dgv_clientes";
-            dgv_clientes.ReadOnly = true;
-            dgv_clientes.RowHeadersVisible = false;
-            dgv_clientes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dgv_clientes.Size = new Size(994, 293);
-            dgv_clientes.TabIndex = 3;
+            btn_buscar.BackColor = Color.FromArgb(0, 0, 192);
+            btn_buscar.FlatAppearance.BorderSize = 0;
+            btn_buscar.Font = new Font("Segoe UI", 11.25F);
+            btn_buscar.ForeColor = SystemColors.ControlLightLight;
+            btn_buscar.Location = new Point(848, 85);
+            btn_buscar.Name = "btn_buscar";
+            btn_buscar.Size = new Size(75, 27);
+            btn_buscar.TabIndex = 14;
+            btn_buscar.Text = "Buscar";
+            btn_buscar.UseVisualStyleBackColor = false;
+            btn_buscar.Click += btn_buscar_Click;
             // 
-            // gpb_lista
+            // btn_novo
             // 
-            gpb_lista.Controls.Add(pnl_edicao);
-            gpb_lista.Controls.Add(dgv_clientes);
-            gpb_lista.Location = new Point(33, 201);
-            gpb_lista.Name = "gpb_lista";
-            gpb_lista.Size = new Size(1019, 321);
-            gpb_lista.TabIndex = 8;
-            gpb_lista.TabStop = false;
-            gpb_lista.Text = "Clientes";
+            btn_novo.BackColor = Color.MediumSeaGreen;
+            btn_novo.FlatAppearance.BorderSize = 0;
+            btn_novo.Font = new Font("Segoe UI", 11.25F);
+            btn_novo.ForeColor = SystemColors.ControlLightLight;
+            btn_novo.Location = new Point(733, 53);
+            btn_novo.Name = "btn_novo";
+            btn_novo.Size = new Size(190, 27);
+            btn_novo.TabIndex = 12;
+            btn_novo.Text = "Cadastrar Profissional";
+            btn_novo.UseVisualStyleBackColor = false;
+            btn_novo.Click += btn_novo_Click;
             // 
-            // pnl_edicao
+            // txt_busca_profissional
             // 
-            pnl_edicao.Controls.Add(gp_observacao);
-            pnl_edicao.Controls.Add(cb_alergico);
-            pnl_edicao.Controls.Add(lbl_profissional);
-            pnl_edicao.Controls.Add(comboBox1);
-            pnl_edicao.Controls.Add(cb_genero);
-            pnl_edicao.Controls.Add(lbl_sexo);
-            pnl_edicao.Controls.Add(textBox2);
-            pnl_edicao.Controls.Add(lbl_telefone);
-            pnl_edicao.Controls.Add(lbl_nome);
-            pnl_edicao.Controls.Add(txt_nome);
-            pnl_edicao.Dock = DockStyle.Bottom;
-            pnl_edicao.Location = new Point(3, 15);
-            pnl_edicao.Name = "pnl_edicao";
-            pnl_edicao.Size = new Size(1013, 303);
-            pnl_edicao.TabIndex = 4;
-            pnl_edicao.Visible = false;
+            txt_busca_profissional.Font = new Font("Segoe UI", 11.25F);
+            txt_busca_profissional.Location = new Point(22, 85);
+            txt_busca_profissional.Name = "txt_busca_profissional";
+            txt_busca_profissional.PlaceholderText = "Digite o nome do profissional para busca";
+            txt_busca_profissional.Size = new Size(820, 27);
+            txt_busca_profissional.TabIndex = 11;
             // 
-            // comboBox1
+            // btn_limpar
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(500, 100);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(249, 23);
-            comboBox1.TabIndex = 6;
+            btn_limpar.BackColor = Color.FromArgb(64, 64, 64);
+            btn_limpar.FlatAppearance.BorderSize = 0;
+            btn_limpar.Font = new Font("Segoe UI", 11.25F);
+            btn_limpar.ForeColor = SystemColors.ControlLightLight;
+            btn_limpar.Location = new Point(556, 124);
+            btn_limpar.Name = "btn_limpar";
+            btn_limpar.Size = new Size(78, 31);
+            btn_limpar.TabIndex = 12;
+            btn_limpar.Text = "Limpar";
+            btn_limpar.UseVisualStyleBackColor = false;
+            btn_limpar.Click += btn_limpar_Click;
             // 
-            // cb_genero
+            // btn_cancelar
             // 
-            cb_genero.FormattingEnabled = true;
-            cb_genero.Location = new Point(187, 100);
-            cb_genero.Name = "cb_genero";
-            cb_genero.Size = new Size(285, 23);
-            cb_genero.TabIndex = 5;
+            btn_cancelar.BackColor = Color.Red;
+            btn_cancelar.FlatAppearance.BorderSize = 0;
+            btn_cancelar.Font = new Font("Segoe UI", 11.25F);
+            btn_cancelar.ForeColor = SystemColors.ControlLightLight;
+            btn_cancelar.Location = new Point(475, 124);
+            btn_cancelar.Name = "btn_cancelar";
+            btn_cancelar.Size = new Size(75, 31);
+            btn_cancelar.TabIndex = 11;
+            btn_cancelar.Text = "Cancelar";
+            btn_cancelar.UseVisualStyleBackColor = false;
+            btn_cancelar.Click += btn_cancelar_Click;
             // 
-            // lbl_sexo
+            // btn_salvar
             // 
-            lbl_sexo.AutoSize = true;
-            lbl_sexo.Location = new Point(187, 82);
-            lbl_sexo.Name = "lbl_sexo";
-            lbl_sexo.Size = new Size(34, 15);
-            lbl_sexo.TabIndex = 4;
-            lbl_sexo.Text = "Sexo:";
+            btn_salvar.BackColor = Color.MediumSeaGreen;
+            btn_salvar.FlatAppearance.BorderSize = 0;
+            btn_salvar.Font = new Font("Segoe UI", 11.25F);
+            btn_salvar.ForeColor = SystemColors.ControlLightLight;
+            btn_salvar.Location = new Point(385, 124);
+            btn_salvar.Name = "btn_salvar";
+            btn_salvar.Size = new Size(84, 31);
+            btn_salvar.TabIndex = 10;
+            btn_salvar.Text = "Salvar";
+            btn_salvar.UseVisualStyleBackColor = false;
+            btn_salvar.Click += btn_salvar_Click;
             // 
-            // textBox2
+            // lbl_titulo
             // 
-            textBox2.Location = new Point(500, 56);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(249, 23);
-            textBox2.TabIndex = 3;
+            lbl_titulo.AutoSize = true;
+            lbl_titulo.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            lbl_titulo.Location = new Point(12, 9);
+            lbl_titulo.Name = "lbl_titulo";
+            lbl_titulo.Size = new Size(365, 32);
+            lbl_titulo.TabIndex = 10;
+            lbl_titulo.Text = "Gerenciamento de Profissional";
+            lbl_titulo.UseMnemonic = false;
+            // 
+            // txt_telefone
+            // 
+            txt_telefone.Font = new Font("Segoe UI", 11.25F);
+            txt_telefone.Location = new Point(385, 78);
+            txt_telefone.Name = "txt_telefone";
+            txt_telefone.Size = new Size(249, 27);
+            txt_telefone.TabIndex = 3;
             // 
             // lbl_telefone
             // 
             lbl_telefone.AutoSize = true;
-            lbl_telefone.Location = new Point(500, 38);
+            lbl_telefone.Font = new Font("Segoe UI", 11.25F);
+            lbl_telefone.Location = new Point(385, 60);
             lbl_telefone.Name = "lbl_telefone";
-            lbl_telefone.Size = new Size(55, 15);
+            lbl_telefone.Size = new Size(69, 20);
             lbl_telefone.TabIndex = 2;
             lbl_telefone.Text = "Telefone:";
             // 
             // lbl_nome
             // 
             lbl_nome.AutoSize = true;
-            lbl_nome.Location = new Point(187, 38);
+            lbl_nome.Font = new Font("Segoe UI", 11.25F);
+            lbl_nome.Location = new Point(72, 60);
             lbl_nome.Name = "lbl_nome";
-            lbl_nome.Size = new Size(43, 15);
+            lbl_nome.Size = new Size(53, 20);
             lbl_nome.TabIndex = 1;
             lbl_nome.Text = "Nome:";
             // 
             // txt_nome
             // 
-            txt_nome.Location = new Point(187, 56);
+            txt_nome.Font = new Font("Segoe UI", 11.25F);
+            txt_nome.Location = new Point(72, 78);
             txt_nome.Name = "txt_nome";
-            txt_nome.Size = new Size(285, 23);
+            txt_nome.Size = new Size(285, 27);
             txt_nome.TabIndex = 0;
             // 
-            // button1
+            // pnl_edicao
             // 
-            button1.BackColor = Color.MediumSeaGreen;
-            button1.FlatAppearance.BorderSize = 0;
-            button1.ForeColor = SystemColors.ControlLightLight;
-            button1.Location = new Point(977, 132);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 7;
-            button1.Text = "Novo";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click_1;
+            pnl_edicao.Controls.Add(check_status);
+            pnl_edicao.Controls.Add(cb_especialidade);
+            pnl_edicao.Controls.Add(lbl_especialidade);
+            pnl_edicao.Controls.Add(btn_limpar);
+            pnl_edicao.Controls.Add(btn_cancelar);
+            pnl_edicao.Controls.Add(btn_salvar);
+            pnl_edicao.Controls.Add(txt_telefone);
+            pnl_edicao.Controls.Add(lbl_telefone);
+            pnl_edicao.Controls.Add(lbl_nome);
+            pnl_edicao.Controls.Add(txt_nome);
+            pnl_edicao.Dock = DockStyle.Bottom;
+            pnl_edicao.Location = new Point(3, 22);
+            pnl_edicao.Name = "pnl_edicao";
+            pnl_edicao.Size = new Size(895, 289);
+            pnl_edicao.TabIndex = 4;
+            pnl_edicao.Visible = false;
             // 
-            // textBox1
+            // check_status
             // 
-            textBox1.Location = new Point(33, 161);
-            textBox1.Name = "textBox1";
-            textBox1.PlaceholderText = "Digite o nome do cliente para busca";
-            textBox1.Size = new Size(1019, 23);
-            textBox1.TabIndex = 6;
+            check_status.AutoSize = true;
+            check_status.Font = new Font("Segoe UI", 11.25F);
+            check_status.Location = new Point(649, 80);
+            check_status.Name = "check_status";
+            check_status.Size = new Size(63, 24);
+            check_status.TabIndex = 16;
+            check_status.Text = "Ativo";
+            check_status.UseVisualStyleBackColor = true;
             // 
-            // lbl_titulo
+            // cb_especialidade
             // 
-            lbl_titulo.AutoSize = true;
-            lbl_titulo.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lbl_titulo.Location = new Point(460, 88);
-            lbl_titulo.Name = "lbl_titulo";
-            lbl_titulo.Size = new Size(81, 25);
-            lbl_titulo.TabIndex = 5;
-            lbl_titulo.Text = "Clientes";
-            lbl_titulo.UseMnemonic = false;
+            cb_especialidade.Font = new Font("Segoe UI", 11.25F);
+            cb_especialidade.FormattingEnabled = true;
+            cb_especialidade.Location = new Point(72, 127);
+            cb_especialidade.Name = "cb_especialidade";
+            cb_especialidade.Size = new Size(285, 28);
+            cb_especialidade.TabIndex = 15;
             // 
-            // lbl_profissional
+            // lbl_especialidade
             // 
-            lbl_profissional.AutoSize = true;
-            lbl_profissional.Location = new Point(500, 82);
-            lbl_profissional.Name = "lbl_profissional";
-            lbl_profissional.Size = new Size(71, 15);
-            lbl_profissional.TabIndex = 7;
-            lbl_profissional.Text = "Profissional:";
+            lbl_especialidade.AutoSize = true;
+            lbl_especialidade.Font = new Font("Segoe UI", 11.25F);
+            lbl_especialidade.Location = new Point(72, 108);
+            lbl_especialidade.Name = "lbl_especialidade";
+            lbl_especialidade.Size = new Size(104, 20);
+            lbl_especialidade.TabIndex = 14;
+            lbl_especialidade.Text = "Especialidade:";
             // 
-            // cb_alergico
+            // gpb_lista
             // 
-            cb_alergico.AutoSize = true;
-            cb_alergico.Location = new Point(187, 142);
-            cb_alergico.Name = "cb_alergico";
-            cb_alergico.Size = new Size(135, 19);
-            cb_alergico.TabIndex = 8;
-            cb_alergico.Text = "Alérgico a produtos?";
-            cb_alergico.UseVisualStyleBackColor = true;
+            gpb_lista.Controls.Add(pnl_edicao);
+            gpb_lista.Controls.Add(dgv_profissional);
+            gpb_lista.Location = new Point(22, 133);
+            gpb_lista.Name = "gpb_lista";
+            gpb_lista.Size = new Size(901, 314);
+            gpb_lista.TabIndex = 13;
+            gpb_lista.TabStop = false;
+            gpb_lista.Text = "Profissionais:";
             // 
-            // txt_produto_alergico
+            // dgv_profissional
             // 
-            txt_produto_alergico.Location = new Point(6, 22);
-            txt_produto_alergico.Name = "txt_produto_alergico";
-            txt_produto_alergico.Size = new Size(562, 23);
-            txt_produto_alergico.TabIndex = 9;
+            dgv_profissional.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv_profissional.BackgroundColor = SystemColors.Control;
+            dgv_profissional.BorderStyle = BorderStyle.None;
+            dgv_profissional.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_profissional.Location = new Point(6, 22);
+            dgv_profissional.Name = "dgv_profissional";
+            dgv_profissional.ReadOnly = true;
+            dgv_profissional.RowHeadersVisible = false;
+            dgv_profissional.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv_profissional.Size = new Size(808, 293);
+            dgv_profissional.TabIndex = 3;
+            dgv_profissional.CellContentClick += dgv_profissional_CellContentClick;
             // 
-            // gp_observacao
-            // 
-            gp_observacao.Controls.Add(txt_produto_alergico);
-            gp_observacao.Location = new Point(187, 167);
-            gp_observacao.Name = "gp_observacao";
-            gp_observacao.Size = new Size(570, 47);
-            gp_observacao.TabIndex = 10;
-            gp_observacao.TabStop = false;
-            gp_observacao.Text = "Quais produtos?";
-            // 
-            // FormCliente
+            // FormProfissional
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1084, 611);
-            Controls.Add(gpb_lista);
-            Controls.Add(button1);
-            Controls.Add(textBox1);
+            ClientSize = new Size(936, 611);
+            Controls.Add(btn_buscar);
+            Controls.Add(btn_novo);
+            Controls.Add(txt_busca_profissional);
             Controls.Add(lbl_titulo);
-            Name = "FormCliente";
-            Text = "FormCliente";
-            ((System.ComponentModel.ISupportInitialize)dgv_clientes).EndInit();
-            gpb_lista.ResumeLayout(false);
+            Controls.Add(gpb_lista);
+            Name = "FormProfissional";
+            Text = "Gerenciamento de Profissionais";
+            Load += Profissional_Load;
             pnl_edicao.ResumeLayout(false);
             pnl_edicao.PerformLayout();
-            gp_observacao.ResumeLayout(false);
-            gp_observacao.PerformLayout();
+            gpb_lista.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgv_profissional).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private DataGridView dgv_clientes;
-        private GroupBox gpb_lista;
-        private Panel pnl_edicao;
-        private Button button1;
-        private TextBox textBox1;
+        private Button btn_buscar;
+        private Button btn_novo;
+        private TextBox txt_busca_profissional;
+        private Button btn_limpar;
+        private Button btn_cancelar;
+        private Button btn_salvar;
         private Label lbl_titulo;
-        private Label lbl_sexo;
-        private TextBox textBox2;
+        private TextBox txt_telefone;
         private Label lbl_telefone;
         private Label lbl_nome;
         private TextBox txt_nome;
-        private ComboBox comboBox1;
-        private ComboBox cb_genero;
-        private CheckBox cb_alergico;
-        private Label lbl_profissional;
-        private GroupBox gp_observacao;
-        private TextBox txt_produto_alergico;
+        private Panel pnl_edicao;
+        private Label lbl_especialidade;
+        private GroupBox gpb_lista;
+        private DataGridView dgv_profissional;
+        private ComboBox cb_especialidade;
+        private CheckBox check_status;
     }
 }
